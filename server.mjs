6ef154330bd925772ccb8252ef9e3ce2b584ea58
Darlley Brito <darlleybrito@gmail.com>
2024-09-03 13:31:26 -0400

    fix: update

--- server.mjs
+++ server.mjs
@@ -1,68 +1,60 @@
 import { StringOutputParser } from '@langchain/core/output_parsers';
 import { PromptTemplate } from '@langchain/core/prompts';
-import { RunnableSequence } from '@langchain/core/runnables';
+import { RunnablePassthrough, RunnableSequence } from '@langchain/core/runnables';
 import { ChatOpenAI } from '@langchain/openai';
 import Fastify from 'fastify';
 
-const openAIApiKey = ''
-const llm = new ChatOpenAI({ openAIApiKey });
+import dotenv from 'dotenv';
+dotenv.config();
 
-// const llm = new FakeListChatModel({
-//   responses: ["i don't liked mondays", "I don't like Mondays"],
-// });
+const openAIApiKey = process.env.OPEN_API_KEY;
+const llm = new ChatOpenAI({ openAIApiKey });
 
 const fastify = Fastify();
 
 // Declare a route
 fastify.get('/', async function handler(request, reply) {
   const punctuationTemplate = `Given a sentence, add punctuation where needed. 
-      sentence: {sentence}
-      sentence with punctuation:  
-    `;
-  const punctuationPrompt = PromptTemplate.fromTemplate(punctuationTemplate);
-
-  const punctuated_sentence = RunnableSequence.from([
-    punctuationPrompt,
-    llm,
-    new StringOutputParser(),
-  ]);
+    sentence: {sentence}
+    sentence with punctuation:
+  `;
 
   const grammarTemplate = `Given a sentence correct the grammar.
-      sentence: {punctuated_sentence}
-      sentence with correct grammar: 
-      `;
-
-  const grammarPrompt = PromptTemplate.fromTemplate(grammarTemplate);
-
-  const grammar_chain = RunnableSequence.from([
-    grammarPrompt,
-    llm,
-    new StringOutputParser(),
-  ]);
+    sentence: {punctuated_sentence}
+    sentence with correct grammar: 
+  `;
 
   const translationTemplate = `Given a sentence, translate that sentence into {language}
-      sentence: {grammar_chain}
-      translated sentence:
+    sentence: {grammatically_correct_sentence}
+    translated sentence:
   `;
+  
+  const punctuationPrompt = PromptTemplate.fromTemplate(punctuationTemplate);
+  const grammarPrompt = PromptTemplate.fromTemplate(grammarTemplate);
   const translationPrompt = PromptTemplate.fromTemplate(translationTemplate);
 
-  const translactionChain = RunnableSequence.from([
-    translationPrompt,
-    llm,
-    new StringOutputParser(),
-    (prev) => console.log(prev),
-  ]);
+  const punctuationChain = RunnableSequence.from([ punctuationPrompt, llm, new StringOutputParser() ]);
+  const grammarChain = RunnableSequence.from([ grammarPrompt, llm, new StringOutputParser() ]);
+  const translactionChain = RunnableSequence.from([ translationPrompt, llm, new StringOutputParser() ]);
 
   const chain = RunnableSequence.from([
-    { punctuated_sentence },
-    { grammar_chain },
-    translactionChain,
+    { 
+      punctuated_sentence: punctuationChain,
+      original_input: new RunnablePassthrough()
+    },
+    { 
+      grammatically_correct_sentence: grammarChain,
+      language: ({ original_input }) => original_input.language
+    },
+    translactionChain
   ]);
 
   const response = await chain.invoke({
-    sentence: 'i dont liked mondays',
+    sentence: 'eu ainda num sei falar franceis',
     language: 'french',
   });
+
+  console.log(response)
 
   reply.send({ response });
 });
